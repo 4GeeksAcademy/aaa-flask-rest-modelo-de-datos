b93616a6414f45a28f09cdd3cc38828e19b1630a
Armando <164431619+Armando7101@users.noreply.github.com>
2025-04-07 20:59:50 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -9,7 +9,8 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    comments: Mapped[list["Comment"]] = relationship(back_populates="user")
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
@@ -17,3 +18,46 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    # Relationship User - MUCHOS A UNO
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="comments")
+
+    # Relationship Post - MUCHOS A UNO
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+    post: Mapped["Post"] = relationship(back_populates="comments")
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # Relationship User - MUCHOS A UNO
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+    # Relationship Comment - UNO A MUCHOS
+    comments: Mapped[list["Comment"]] = relationship(back_populates="post")
+
+    # Relationship with Media - UNO A MUCHOS
+    media: Mapped[list["Media"]] = relationship(back_populates="post")
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(String(120), nullable=False)
+    url: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    # Relationship with Post - MUCHOS A UNO
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+    post: Mapped["Post"] = relationship(back_populates="media")
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
+
+    user_to_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
